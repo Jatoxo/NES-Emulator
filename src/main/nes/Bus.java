68764026d7d1cf4f136f22f52c31004bf78ef7e1
Jatoxo <39669932+Jatoxo@users.noreply.github.com>
2023-10-12 01:14:10 +0200

    Cache CPU Bus devices in array
    ArrayList is incredibly slow

--- src/main/nes/Bus.java
+++ src/main/nes/Bus.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 
 public class Bus {
 
-	ArrayList<BusDevice> busDevices;
+	private final ArrayList<BusDevice> busDevices;
 
-
+	//Used to quickly access bus devices
+	private BusDevice[] busDevicesArray;
 
 	public Bus() {
 		busDevices = new ArrayList<>();
@@ -15,15 +16,15 @@ public class Bus {
 
 	public void addBusDevice(BusDevice busDevice) {
 		busDevices.add(busDevice);
+		busDevicesArray = busDevices.toArray(new BusDevice[0]);
 	}
 	public void removeBusDevice(BusDevice busDevice) {
 		busDevices.remove(busDevice);
+		busDevicesArray = busDevices.toArray(new BusDevice[0]);
 	}
 
 	public void write(int addr, int data) { //16-Bit address, 8-Bit Data
-
-		for(BusDevice device : busDevices) {
-
+		for(BusDevice device : busDevicesArray) {
 			if(addr >= device.addrStart && addr <= device.addrEnd) {
 				device.write(addr, data & 0xFF);
 				return;
@@ -33,7 +34,7 @@ public class Bus {
 	}
 
 	public int read(int addr, boolean bReadOnly) {
-		for(BusDevice device : busDevices) {
+		for(BusDevice device : busDevicesArray) {
 			if(addr >= device.addrStart && addr <= device.addrEnd) {
 				return device.read(addr) & 0xFF;
 			}
